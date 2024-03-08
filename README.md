95f3b51fb038076217437f407b3484bedacbac8c
Ericzeppe53 <156183859+Ericzeppe53@users.noreply.github.com>
2024-03-08 13:41:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eric-project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Ericzeppe53/Eric-project)
---
+++ script.js
@@ -0,0 +1,18 @@
+function setStage(newStage) {
+  // Hent ut form elementet
+  const form = document.querySelector("#multi-step-form")
+
+  // // Fjerner det nåværende css klassen
+  form.classList.remove("current-form-1")
+  form.classList.remove("current-form-2")
+  form.classList.remove("current-form-3")
+  form.classList.remove("current-form-4")
+
+  // // Legg til den nye css klassen
+  const newClass = "current-form-" + newStage
+  form.classList.add(newClass)
+}
+
+function submitForm() {
+  console.log("Submitting form")
+}
